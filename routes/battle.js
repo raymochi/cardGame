d068be001073a321dba1e03a57d15ec1321efcf8
ffiargus <rm.chow@outlook.com>
2017-05-25 00:16:21 +0000

    adds express routes and empty files

--- routes/battle.js
+++ routes/battle.js
@@ -1,9 +1,22 @@
 "use strict";
 
 const express = require('express');
+const stringGen   = require('./lib/stringGenerator');
 const battleRoutes  = express.Router();
 
+let matchId = null;
+
 module.exports = (knex) => {
+
+  battleRoutes.get('/', (req, res) => {
+    if (!matchId) {
+      matchId = stringGen();
+      res.redirect(`/${matchId}`);
+    } else {
+      res.redirect(`/${matchId}`);
+      matchId = null;
+    }
+  });
 
   //standard get request for loading battle page after
   //clicking play
